1bc5b37478c23536ede713be66fe10f430d574d8
asifmu97 <usman128399@gmail.com>
2024-11-13 11:55:01 -0600

    add toolbar to layout and add container to css

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
+import ToolBar from "../Toolbar/Toolbar";
 import "./Layout.css";
 
 export interface LayoutContext {
@@ -10,16 +11,32 @@ export interface LayoutContext {
 function Layout() {
 	const [searchQuery, searchQueryChange] = React.useState("");
 
-	return (
-		<div className="full-screen">
-			<Header
-				searchQuery={searchQuery}
-				searchQueryChange={(val) => searchQueryChange(val)}
-			></Header>
-			<Outlet context={{ searchQuery }} />
-			{/* Add toolbar here*/}
-		</div>
-	);
+	// return (
+	// 	<div className="full-screen">
+	// 		<Header
+	// 			searchQuery={searchQuery}
+	// 			searchQueryChange={(val) => searchQueryChange(val)}
+	// 		></Header>
+	// 		<Outlet context={{ searchQuery }} />
+	// 		{/* Add toolbar here*/}
+    //         <ToolBar/>
+	// 	</div>
+	// );
+
+    return (
+        <div className="full-screen">
+          <Header
+            searchQuery={searchQuery}
+            searchQueryChange={(val) => searchQueryChange(val)}
+          />
+          
+          <div className="content-container">
+            <Outlet context={{ searchQuery }} />
+          </div>
+    
+          <ToolBar />
+        </div>
+      );
 }
 
 export default Layout;
