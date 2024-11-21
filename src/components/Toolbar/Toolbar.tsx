00cdc7115dd679910c3a22fc6f493866641921b4
asifmu97 <usman128399@gmail.com>
2024-11-20 23:27:48 -0600

    change folder structure and code therein

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import homeIcon from "../../assets/icons/Home/home_col.png";
-import ingredientIcon from "../../assets/icons/Ingredients/ingredients_col.png";
-import listIcon from "../../assets/icons/My Lists/myLists_col.png";
-import recipeIcon from "../../assets/icons/Recipes/recipe_col.png";
+import homeIcon from "../../assets/icons/ToolBar/Home/home_col.png";
+import ingredientIcon from "../../assets/icons/ToolBar/Ingredients/ingredients_col.png";
+import listIcon from "../../assets/icons/ToolBar/My Lists/myLists_col.png";
+import recipeIcon from "../../assets/icons/ToolBar/Recipes/recipe_col.png";
 import "./Toolbar.css";
 
 function ToolBar() {
