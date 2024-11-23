5f32a3ab941f5aa58b685c656e981b859179440b
Hien Nguyen <hiennguyen.wcs@gmail.com>
2024-11-23 17:31:43 -0600

    Highlight selected button

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import homeIcon from "../../assets/icons/ToolBar/Home/home_col.png";
 import ingredientIcon from "../../assets/icons/ToolBar/Ingredients/ingredients_col.png";
 import listIcon from "../../assets/icons/ToolBar/My Lists/myLists_col.png";
@@ -7,42 +7,71 @@ import recipeIcon from "../../assets/icons/ToolBar/Recipes/recipe_col.png";
 import "./Toolbar.css";
 
 function ToolBar() {
+	const navigate = useNavigate();
+	const isDisabled = React.useState(true);
+	const location = useLocation();
 
-    const navigate = useNavigate();
-    const isDisabled = React.useState(true);
+	// To make button looks selected while on that page
+	const isActive = (path: string) => location.pathname === path;
 
-    return (
-        <div className="toolbar">
-          <div
-            className="toolbar-button"
-            onClick={() => navigate("/logged-in")}
-          >
-            <img src={homeIcon} alt="Home" className="toolbar-button-icon" />
-            <span className="toolbar-button-text">Home</span>
-          </div>
-    
-          <div
-            className="toolbar-button"
-            onClick={() => navigate("/my-lists")}
-          >
-            <img src={listIcon} alt="MyLists" className="toolbar-button-icon" />
-            <span className="toolbar-button-text">My Lists</span>
-          </div>
-    
-          <div
-            className="toolbar-button"
-            onClick={() => navigate("/all-ingredients")}
-          >
-            <img src={ingredientIcon} alt="AllIngredients" className="toolbar-button-icon" />
-            <span className="toolbar-button-text">Ingredients</span>
-          </div>
-    
-          <div className={`toolbar-button ${isDisabled ? "disabled" : ""}`}>
-            <img src={recipeIcon} alt="Recipes" className="toolbar-button-icon" />
-            <span className="toolbar-button-text">Recipes</span>
-          </div>
-        </div>
-      );
+	const goToPage = (path: string) => {
+		if (location.pathname !== path) {
+			navigate(path);
+		}
+	};
+
+	return (
+		<div className="toolbar">
+			<div
+				className={`toolbar-button ${isActive("/logged-in") ? "selected" : ""}`}
+				onClick={() => goToPage("/logged-in")}
+			>
+				<img
+					src={homeIcon}
+					alt="Home"
+					className="toolbar-button-icon"
+				/>
+				<span className="toolbar-button-text">Home</span>
+			</div>
+
+			<div
+				className={`toolbar-button ${isActive("/my-lists") ? "selected" : ""}`}
+				onClick={() => goToPage("/my-lists")}
+			>
+				<img
+					src={listIcon}
+					alt="MyLists"
+					className="toolbar-button-icon"
+				/>
+				<span className="toolbar-button-text">My Lists</span>
+			</div>
+
+			<div
+				className={`toolbar-button ${isActive("/all-ingredients") ? "selected" : ""}`}
+				onClick={() => goToPage("/all-ingredients")}
+			>
+				<img
+					src={ingredientIcon}
+					alt="AllIngredients"
+					className="toolbar-button-icon"
+				/>
+				<span className="toolbar-button-text">Ingredients</span>
+			</div>
+
+			<div
+				className={`toolbar-button 
+                ${isActive("/recipes") ? "selected" : ""} 
+                ${isDisabled ? "disabled" : ""}`}
+			>
+				<img
+					src={recipeIcon}
+					alt="Recipes"
+					className="toolbar-button-icon"
+				/>
+				<span className="toolbar-button-text">Recipes</span>
+			</div>
+		</div>
+	);
 }
 
 export default ToolBar;
