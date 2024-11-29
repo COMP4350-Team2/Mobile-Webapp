baa6328a3054c5d831da9c64b2eaa9a1ccf1fafd
asifmu97 <usman128399@gmail.com>
2024-11-28 22:14:37 -0600

    add filter icon to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
-import { AppBar, IconButton, Input, InputAdornment, Toolbar, Typography } from "@mui/material";
+import FilterListIcon from '@mui/icons-material/FilterList';
+import { AppBar, Box, Checkbox, FormControlLabel, IconButton, Input, InputAdornment, Menu, MenuItem, Toolbar, Typography } from "@mui/material";
 import { UserAuth } from "auth/UserAuth";
-import SideMenu from "components/SideMenu/SideMenu";
-import React, { useEffect, useMemo, useRef } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import { AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
 import { useLocation } from "react-router-dom";
 import "./Header.css";
@@ -10,18 +10,22 @@ interface HeaderProp {
 	userAuth: UserAuth;
 	searchQuery: string;
 	searchQueryChange: (val: string) => void;
+    setFilter: React.Dispatch<React.SetStateAction<'All' | 'Common' | 'Custom'>>;
+    filter: 'All' | 'Common' | 'Custom';
 }
 
-function Header({ userAuth, searchQuery, searchQueryChange }: HeaderProp) {
+function Header({ userAuth, searchQuery, searchQueryChange, setFilter, filter }: HeaderProp) {
 	const [menuOpenned, toggleMenu] = React.useState(false);
-	const [searchBarOpenned, toggleSearchBar] = React.useState(false);
+	const [searchBarOpenned, toggleSearchBar] = useState(false);
 	const location = useLocation();
 	const prevScreenName = useRef<string | null>(null);
+    const [filterOptionsVisible, setFilterOptionsVisible] = useState(false);
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
 	const routeNames = useMemo(
 		() => ({
 			"/home": "Home",
-			"/all-ingredients": "Ingredient Selections",
+			"/all-ingredients": "Ingredients",
 			"/my-lists": "My Lists",
 			"/view-list/:listName": ":listName",
 		}),
@@ -58,13 +62,23 @@ function Header({ userAuth, searchQuery, searchQueryChange }: HeaderProp) {
 		prevScreenName.current = activeScreenName;
 	}, [activeScreenName, searchInapplicableScreens, prevScreenName, searchQueryChange]);
 
+    //ensuring the filters can be dynamically used with search queries
+    useEffect(() => {}, [filter, searchQueryChange]);
+
+    //resetting the filter option when re-rendering
+    useEffect(() => {
+        setFilter("All");
+        setFilterOptionsVisible(activeScreenName === "Ingredients");
+    }, [location.pathname, setFilter, activeScreenName]);
+
+
 	return (
 		<>
 			<AppBar
 				position="static"
 				className="header-color"
 			>
-				<Toolbar>
+				<Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
 					<IconButton
 						aria-label="menu"
 						aria-controls={menuOpenned ? "menu" : undefined}
@@ -75,16 +89,16 @@ function Header({ userAuth, searchQuery, searchQueryChange }: HeaderProp) {
 					>
 						<AiOutlineMenu />
 					</IconButton>
-					<Typography
-						variant="h6"
-						style={{
-							flexGrow: 1,
-							textAlign: "center",
-							color: "white",
-						}}
-					>
-						{activeScreenName}
-					</Typography>
+                    <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
+                        <Typography
+                            variant="h6"
+                            style={{
+                                color: "white",
+                            }}
+                        >
+                            {activeScreenName}
+                        </Typography>
+                    </Box>
 					{!searchInapplicableScreens.includes(activeScreenName) ? (
 						<IconButton
 							aria-label="menu"
@@ -103,6 +117,115 @@ function Header({ userAuth, searchQuery, searchQueryChange }: HeaderProp) {
 				</Toolbar>
 			</AppBar>
 
+            {filterOptionsVisible && activeScreenName === "Ingredients" && (
+            <Box
+                sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    backgroundColor: "#3282b8",
+                    padding: "8px",
+                    color: "white",
+                }}
+            >
+                {/* Filter Dropdown */}
+                <Box sx={{ marginRight: "auto" }}>
+                    <button
+                        onClick={(event) => setAnchorEl(event.currentTarget)}
+                        style={{
+                            backgroundColor: "white",
+                            color: "#0f4c75",
+                            fontWeight: "bold",
+                            padding: "6px 12px",
+                            border: "none",
+                            borderRadius: "30px",
+                            cursor: "pointer",
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            height: 30,
+                        }}
+                    >
+                        <FilterListIcon sx={{ marginRight: "8px" }} /> Filter
+                    </button>
+                    <Menu
+                        anchorEl={anchorEl}
+                        open={Boolean(anchorEl)}
+                        onClose={() => setAnchorEl(null)}
+                        PaperProps={{
+                            style: {
+                                backgroundColor: "white",
+                                color: "black",
+                            },
+                        }}
+                    >
+                        <MenuItem
+                            selected={filter === "All"}
+                            onClick={() => {
+                                setFilter("All");
+                                setAnchorEl(null);
+                            }}
+                        >
+                            <FormControlLabel
+                                control={
+                                    <Checkbox
+                                        checked={filter === "All"}
+                                        onChange={() => setFilter("All")}
+                                        sx={{
+                                            color: "#0f4c75",
+                                            "&.Mui-checked": { color: "#0f4c75" },
+                                        }}
+                                    />
+                                }
+                                label="All"
+                            />
+                        </MenuItem>
+                        <MenuItem
+                            selected={filter === "Common"}
+                            onClick={() => {
+                                setFilter("Common");
+                                setAnchorEl(null);
+                            }}
+                        >
+                            <FormControlLabel
+                                control={
+                                    <Checkbox
+                                        checked={filter === "Common"}
+                                        onChange={() => setFilter("Common")}
+                                        sx={{
+                                            color: "#0f4c75",
+                                            "&.Mui-checked": { color: "#0f4c75" },
+                                        }}
+                                    />
+                                }
+                                label="Common"
+                            />
+                        </MenuItem>
+                        <MenuItem
+                            selected={filter === "Custom"}
+                            onClick={() => {
+                                setFilter("Custom");
+                                setAnchorEl(null);
+                            }}
+                        >
+                            <FormControlLabel
+                                control={
+                                    <Checkbox
+                                        checked={filter === "Custom"}
+                                        onChange={() => setFilter("Custom")}
+                                        sx={{
+                                            color: "#0f4c75",
+                                            "&.Mui-checked": { color: "#0f4c75" },
+                                        }}
+                                    />
+                                }
+                                label="Custom"
+                            />
+                        </MenuItem>
+                    </Menu>
+                </Box>
+            </Box>
+        )}
+
 			{searchBarOpenned && (
 				<Input
 					value={searchQuery}
@@ -117,13 +240,7 @@ function Header({ userAuth, searchQuery, searchQueryChange }: HeaderProp) {
 				/>
 			)}
 
-			{menuOpenned && (
-				<SideMenu
-					userAuth={userAuth}
-					open={menuOpenned}
-					onClose={() => toggleMenu(false)}
-				></SideMenu>
-			)}
+
 		</>
 	);
 }
